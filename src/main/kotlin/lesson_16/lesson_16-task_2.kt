cdde79455f46fc22fe3e00403c824286f9fce7f8
platon <platon@enod.xyz>
2023-10-03 12:39:23 +0500

    KS-16-2- Circle: area, perimeter

--- src/main/kotlin/lesson_16/lesson_16-task_2.kt
+++ src/main/kotlin/lesson_16/lesson_16-task_2.kt
@@ -2,3 +2,15 @@ package lesson_16
 
 /** Created by Platon2025 */
 
+private const val PI = 3.14f
+
+class Circle(private val radius: Int) {
+    fun getArea() = PI * radius * radius
+    fun getPerimeter() = 2 * PI * radius
+}
+
+fun main() {
+    val circle = Circle(5)
+    println("Площадь круга: ${circle.getArea()}")
+    println("Длина окружности: ${circle.getPerimeter()}")
+}
