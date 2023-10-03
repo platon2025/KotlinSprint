7f99e52f200e5eb1a73e6d957f873d47d9b64fd8
platon <platon@enod.xyz>
2023-10-04 01:52:13 +0500

    KS-15-5- Transport : interfaces : Get rid of null

--- src/main/kotlin/lesson_15/lesson_15-task_5.kt
+++ src/main/kotlin/lesson_15/lesson_15-task_5.kt
@@ -4,12 +4,27 @@ package lesson_15
 
 const val MAX_PASSENGERS_CAR = 3
 const val MAX_PASSENGERS_TRUCK = 1
-const val MAX_TONNAGE_TRUCK = 2
+const val MAX_CARGO_TRUCK = 2
 
 interface PassengerTransport {
     val name: String
     val maxPassengers: Int
-    fun go(passengers: Int): Map<String, Int> =
+    fun go(passengers: Int): Map<String, Int>
+}
+
+interface CargoTransport {
+    val name: String
+    val maxPassengers: Int
+    val maxCargo: Int
+    fun go(passengers: Int, cargo: Int): Map<String, Int>
+
+}
+
+class Car(
+    override val name: String,
+    override val maxPassengers: Int = MAX_PASSENGERS_CAR,
+) : PassengerTransport {
+    override fun go(passengers: Int): Map<String, Int> =
         if (passengers <= maxPassengers) {
             println("$name перевез $passengers пассажиров.")
             mapOf(Pair("passengers", passengers))
@@ -17,71 +32,52 @@ interface PassengerTransport {
             println("$name не смог взять на борт $passengers пассажиров. Поездка не состоялась.")
             mapOf(Pair("passengers", 0))
         }
-
 }
-
-interface CargoTransport {
-    val name: String
-    val maxPassengers: Int
-    val maxTonnage: Int
-    fun go(passengers: Int, tonnage: Int): Map<String, Int> =
-        if (passengers <= maxPassengers && tonnage <= maxTonnage) {
-            println("$name перевез $passengers пассажиров и $tonnage тонн груза.")
-            mapOf(Pair("passengers", passengers), Pair("tonnage", tonnage))
-        } else {
-            println("$name не смог взять на борт $passengers пассажиров и $tonnage тонн груза. Поездка не состоялась.")
-            mapOf(Pair("passengers", 0), Pair("tonnage", 0))
-        }
-
-}
-
-class Car(
-    override val name: String,
-    override val maxPassengers: Int = MAX_PASSENGERS_CAR
-) : PassengerTransport
 
 class Truck(
     override val name: String,
     override val maxPassengers: Int = MAX_PASSENGERS_TRUCK,
-    override val maxTonnage: Int = MAX_TONNAGE_TRUCK
-) : CargoTransport
+    override val maxCargo: Int = MAX_CARGO_TRUCK,
+) : CargoTransport {
+    override fun go(passengers: Int, cargo: Int): Map<String, Int> =
+        if (passengers <= maxPassengers && cargo <= maxCargo) {
+            println("$name перевез $passengers пассажиров и $cargo тонн груза.")
+            mapOf(Pair("passengers", passengers), Pair("cargo", cargo))
+        } else {
+            println("$name не смог взять на борт $passengers пассажиров и $cargo тонн груза. Поездка не состоялась.")
+            mapOf(Pair("passengers", 0), Pair("cargo", 0))
+        }
+}
 
 fun main() {
-    val task = mapOf(Pair("passengers", 6), Pair("tonnage", 2))
-    var completed = mapOf(Pair("passengers", 0), Pair("tonnage", 0))
+    val passengersPlan = 6
+    val cargoPlan = 2
+    var transported: Map<String, Int> = mutableMapOf(Pair("passengers", 0), Pair("cargo", 0))
 
-    println("Нужно перевезти ${task["passengers"]} человек и ${task["tonnage"]} тонн груза\n")
-
-    completed = countCompletion(completed, Car("Красный кабриолет").go(2))
-    printTripResult(task, completed)
-    completed = countCompletion(completed, Truck("Зеленый фургон").go(2, 2))
-    printTripResult(task, completed)
-    completed = countCompletion(completed, Car("Чёрный бумер").go(3))
-    printTripResult(task, completed)
-    completed = countCompletion(completed, Truck("Желтый фургон").go(1, 2))
-    printTripResult(task, completed)
-}
-
-fun printTripResult(task: Map<String, Int>, completed: Map<String, Int>) {
-    println(
-        "Осталось перевезти ${task["passengers"]?.minus(completed["passengers"]!!)} человек и ${
-            task["tonnage"]?.minus(
-                completed["tonnage"]!!
-            )
-        } тонн груза.\n"
+    transported = countTransported(
+        trip = Car("Красный кабриолет").go(2),
+        transportedBefore = transported,
+        passengersPlan = passengersPlan,
+        cargoPlan = cargoPlan,
     )
+    countTransported(Truck("Зеленый фургон").go(2, 2), transported, passengersPlan, cargoPlan).also { transported = it }
+    countTransported(Car("Чёрный бумер").go(3), transported, passengersPlan, cargoPlan).also { transported = it }
+    countTransported(Truck("Желтый фургон").go(1, 2), transported, passengersPlan, cargoPlan)
+
 }
 
-fun countCompletion(completedBefore: Map<String, Int>, completed: Map<String, Int>): Map<String, Int> {
-    val totalCompleted = mutableMapOf(Pair("passengers", 0), Pair("tonnage", 0))
-    if (completedBefore.isNotEmpty()) {
-        if (completedBefore["passengers"] != null) totalCompleted["passengers"] = completedBefore["passengers"]!!
-        if (completedBefore["tonnage"] != null) totalCompleted["tonnage"] = completedBefore["tonnage"]!!
-    }
-    if (completed["passengers"] != null && completed["passengers"] != 0) totalCompleted["passengers"] =
-        totalCompleted["passengers"]!! + completed["passengers"]!!
-    if (completed["tonnage"] != null && completed["tonnage"] != 0) totalCompleted["tonnage"] =
-        totalCompleted["tonnage"]!! + completed["tonnage"]!!
+fun countTransported(
+    trip: Map<String, Int>,
+    transportedBefore: Map<String, Int>,
+    passengersPlan: Int,
+    cargoPlan: Int
+): Map<String, Int> {
 
-    return totalCompleted
+    val totalTransportedPassengers = (transportedBefore["passengers"] ?: 0) + (trip["passengers"] ?: 0)
+    val totalTransportedCargo = (transportedBefore["cargo"] ?: 0) + (trip["cargo"] ?: 0)
+
+    if (totalTransportedPassengers >= passengersPlan && totalTransportedCargo >= cargoPlan) println("Jobs done!")
+    else println("Осталось перевезти ${passengersPlan - totalTransportedPassengers} пассажиров и ${cargoPlan - totalTransportedCargo} тонн груза\n")
+
+    return mapOf(Pair("passengers", totalTransportedPassengers), Pair("cargo", totalTransportedCargo))
 }
