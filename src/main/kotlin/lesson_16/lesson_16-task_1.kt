bfd03a9bce380c13637028a5b2178b8b1c6f3f8f
platon <platon@enod.xyz>
2023-10-03 12:27:12 +0500

    KS-16-1- Dice.getScore()

--- src/main/kotlin/lesson_16/lesson_16-task_1.kt
+++ src/main/kotlin/lesson_16/lesson_16-task_1.kt
@@ -1,4 +1,14 @@
 package lesson_16
 
 /** Created by Platon2025 */
+const val MIN_SCORE = 1
+const val MAX_SCORE = 6
 
+class Dice(private val score: Int = (MIN_SCORE..MAX_SCORE).random()) {
+    fun getScore() = score
+}
+
+fun main() {
+    val dice = Dice()
+    println("${dice.getScore()}")
+}
