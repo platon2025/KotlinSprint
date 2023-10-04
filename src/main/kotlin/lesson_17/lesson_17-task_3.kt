eff14497fa0a0731274d50a73258857c4009d63a
platon <platon@enod.xyz>
2023-10-04 23:04:10 +0500

    KS-17-3- Hidden Folder

--- src/main/kotlin/lesson_17/lesson_17-task_3.kt
+++ src/main/kotlin/lesson_17/lesson_17-task_3.kt
@@ -1,4 +1,20 @@
 package lesson_17
 
 /** Created by Platon2025 */
+const val SECRET_FOLDER_ALERT: String = "скрытая папка"
+const val SECRET_FOLDER_FILES_NUM = 0
 
+class Folder(name: String, numberOfFiles: Int, var isHidden: Boolean) {
+    var name = name
+        get() = if (!isHidden) field else SECRET_FOLDER_ALERT
+
+    var numberOfFiles = numberOfFiles
+        get() = if (!isHidden) field else SECRET_FOLDER_FILES_NUM
+}
+
+fun main() {
+    val folder = Folder("Мои Документы", 100500, false)
+    println("Папка \"${folder.name}\" содержит ${folder.numberOfFiles} файлов")
+    folder.isHidden = true
+    println("Папка \"${folder.name}\" содержит ${folder.numberOfFiles} файлов")
+}
