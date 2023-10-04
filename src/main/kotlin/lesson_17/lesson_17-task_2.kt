a12b7b99c3fc4753b1b3662378548fae52d884c2
platon <platon@enod.xyz>
2023-10-04 22:29:39 +0500

    KS-17-2- Ship : setter

--- src/main/kotlin/lesson_17/lesson_17-task_2.kt
+++ src/main/kotlin/lesson_17/lesson_17-task_2.kt
@@ -2,17 +2,22 @@ package lesson_17
 
 /** Created by Platon2025 */
 
-class Ship(name: String, val averageSpeed: Int, val homePort: String) {
+class Ship(name: String, val averageSpeed: Int, homePort: String) {
     var name: String = name
         set(value) {
             println("Имя корабля нельзя изменить!")
         }
+    var homePort: String = homePort
+        set(value) {
+            field = value
+        }
 }
 
 fun main() {
-    var ship = Ship("Черная жемчужина", 30, "Тартуга")
-    println("Имя корабля: ${ship.name}")
+    val ship = Ship("Черная жемчужина", 30, "Тартуга")
+    println("Имя корабля: ${ship.name}, порт приписки: ${ship.homePort}")
     ship.name = "Летучий голландец"
-    println("Имя корабля: ${ship.name}")
+    ship.homePort = "Порт-Роял"
+    println("Имя корабля: ${ship.name}, порт приписки: ${ship.homePort}")
 
 }
