afc5d72cc5c8d07431a5bc8c01d6a56e92ea133f
platon <platon@enod.xyz>
2023-10-04 21:53:19 +0500

    KS-16-4- Order status changed by admin

--- src/main/kotlin/lesson_16/lesson_16-task_4.kt
+++ src/main/kotlin/lesson_16/lesson_16-task_4.kt
@@ -2,17 +2,23 @@ package lesson_16
 
 /** Created by Platon2025 */
 
-class Order(val id: Int, private var status: String) {
+class Order(private val orderNumber: Int, private var status: String) {
     fun changeOrderStatus(newStatus: String) {
+        setNewOrderStatus(newStatus)
+        println("Статус заказа #$orderNumber меняется на \"$newStatus\"")
+    }
+
+    private fun setNewOrderStatus(newStatus: String) {
         status = newStatus
     }
 
     fun getOrderStatus(): String = status
+    fun getOrderNumber(): Int = orderNumber
 }
 
 fun main() {
     val order = Order(1112, "Размещён")
-    println("Статус заказа #${order.id}: ${order.getOrderStatus()}")
+    println("Статус заказа #${order.getOrderNumber()}: ${order.getOrderStatus()}")
     order.changeOrderStatus("Оплачен")
-    println("Статус заказа #${order.id}: ${order.getOrderStatus()}")
+    println("Статус заказа #${order.getOrderNumber()}: ${order.getOrderStatus()}")
 }
