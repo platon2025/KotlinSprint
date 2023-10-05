e978257d5d0ef2dcc9e25954bf70f55fdcd19def
platon <platon@enod.xyz>
2023-10-05 14:50:20 +0500

    KS-18-1- Polymorphism : Ad hock

--- src/main/kotlin/lesson_18/lesson_18-task_1.kt
+++ src/main/kotlin/lesson_18/lesson_18-task_1.kt
@@ -2,3 +2,19 @@ package lesson_18
 
 /** Created by Platon2025 */
 
+class Order() {
+    fun printOrderInfo(orderNumber: Int, orderContent: String) {
+        println("Заказ #$orderNumber. Заказан товар: $orderContent.")
+    }
+
+    fun printOrderInfo(orderNumber: Int, orderContent: List<String>) {
+        println("Заказ #$orderNumber. Заказаны следующие товары: ${orderContent.joinToString()}.")
+    }
+}
+
+fun main() {
+    val order = Order()
+
+    order.printOrderInfo(54, "Телескоп")
+    order.printOrderInfo(113, listOf("Зубочистки, Пассатижи"))
+}
