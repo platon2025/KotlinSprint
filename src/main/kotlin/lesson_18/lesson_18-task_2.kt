97c2049dced370d047c929944a3e8d3edcc98b80
platon <platon@enod.xyz>
2023-10-05 15:45:59 +0500

    KS-18-2- Polymorphism : Subtyping

--- src/main/kotlin/lesson_18/lesson_18-task_2.kt
+++ src/main/kotlin/lesson_18/lesson_18-task_2.kt
@@ -2,3 +2,28 @@ package lesson_18
 
 /** Created by Platon2025 */
 
+open class Dice() {
+    open val facesNum: Int = 1
+    fun getScore() = (1..facesNum).random()
+}
+
+class DiceOf4() : Dice() {
+    override val facesNum = 4
+}
+
+class DiceOf6() : Dice() {
+    override val facesNum = 6
+}
+
+class DiceOf8() : Dice() {
+    override val facesNum = 8
+}
+
+fun main() {
+    val dice4: Dice = DiceOf4()
+    val dice6: Dice = DiceOf6()
+    val dice8: Dice = DiceOf8()
+    println(dice4.getScore())
+    println(dice6.getScore())
+    println(dice8.getScore())
+}
