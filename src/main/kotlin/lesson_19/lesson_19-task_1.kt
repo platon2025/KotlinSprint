61148c69c7ba17186c18129d7dc1dbda2ad28bed
platon <platon@enod.xyz>
2023-10-08 11:20:21 +0500

    KS-19-1- Enum class: List entries

--- src/main/kotlin/lesson_19/lesson_19-task_1.kt
+++ src/main/kotlin/lesson_19/lesson_19-task_1.kt
@@ -2,3 +2,18 @@ package lesson_19
 
 /** Created by Platon2025 */
 
+enum class AquariumFish(var fullName: String) {
+    GUPPI("Guppi"),
+    ANGEL("Angel fish"),
+    GOLD("Gold fish"),
+    SIAMESE("Siamese fighting fish"),
+}
+
+fun listAquariumDwellers() {
+    AquariumFish.entries.forEach { println(it.fullName) }
+}
+
+fun main() {
+    println("Вы можете добавить в свой аквариум следующих обитателей:")
+    listAquariumDwellers()
+}
