5bb4afeaefd46eff57bd5c9eed78c46a556cfd57
platon <platon@enod.xyz>
2023-10-08 18:56:36 +0500

    KS-19-3- TODO

--- src/main/kotlin/lesson_19/lesson_19-task_3.kt
+++ src/main/kotlin/lesson_19/lesson_19-task_3.kt
@@ -2,3 +2,26 @@ package lesson_19
 
 /** Created by Platon2025 */
 
+class SpaceShip {
+    fun takeOff() {
+        TODO()
+    }
+
+    fun landing() {
+        println("Звездолёт припаркован")
+    }
+
+    fun shoot() {
+        println("Огонь по выбранной цели")
+        //TODO : прописать логику с учетом выбранного оружия
+    }
+}
+
+fun main() {
+    val spaceShip = SpaceShip()
+
+//    spaceShip.takeOff()
+    spaceShip.shoot()
+    spaceShip.landing()
+
+}
