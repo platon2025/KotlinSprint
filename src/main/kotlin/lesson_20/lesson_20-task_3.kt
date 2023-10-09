1e1d6cb9d92c6f954123ec357f57d2e99da25a34
platon <platon@enod.xyz>
2023-10-09 12:43:26 +0500

    KS-20-3- Lambda : verifyKeyFound

--- src/main/kotlin/lesson_20/lesson_20-task_3.kt
+++ src/main/kotlin/lesson_20/lesson_20-task_3.kt
@@ -2,3 +2,18 @@ package lesson_20
 
 /** Created by Platon2025 */
 
+class User2() {
+    var isKeyFound = false
+
+    val verifyKeyFound = {
+        if (isKeyFound) println("Дверь открыта")
+        else println("Дверь закрыта")
+    }
+}
+
+fun main() {
+    val user = User2()
+    user.verifyKeyFound()
+    user.isKeyFound = true
+    user.verifyKeyFound()
+}
