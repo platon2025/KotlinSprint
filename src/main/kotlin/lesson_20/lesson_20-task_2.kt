2aa436680052b535f7f24744dda7b7d04a5a9756
platon <platon@enod.xyz>
2023-10-09 12:23:10 +0500

    KS-20-2- Lambda : healing potion

--- src/main/kotlin/lesson_20/lesson_20-task_2.kt
+++ src/main/kotlin/lesson_20/lesson_20-task_2.kt
@@ -1,4 +1,18 @@
 package lesson_20
 
 /** Created by Platon2025 */
+const val MAX_HEALTH = 100
 
+class User(val name: String, var health: Int) {
+    val healingPotion = {
+        this.health = MAX_HEALTH
+        println("Лечение игрока $name проведено. Текущий уровень здоровья: $MAX_HEALTH")
+    }
+}
+
+fun main() {
+    val user = User("Duke Nukem", 70)
+    println("Текущий уровень здоровья игрока ${user.name} - ${user.health}")
+    user.healingPotion()
+    println("Текущий уровень здоровья игрока ${user.name} - ${user.health}")
+}
