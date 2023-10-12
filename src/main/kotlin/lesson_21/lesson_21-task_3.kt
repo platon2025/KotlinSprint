98585581fa41961b3e8f2bd85f00fef375a7a5dc
platon <platon@enod.xyz>
2023-10-13 01:52:30 +0500

    KS-21-3 - Player.isHealthy()

--- src/main/kotlin/lesson_21/lesson_21-task_3.kt
+++ src/main/kotlin/lesson_21/lesson_21-task_3.kt
@@ -1,3 +1,19 @@
 package lesson_21
 
+const val MAX_HEALTH = 100
 
+class Player(val name: String, var health: Int) {
+    val healingPotion = {
+        this.health = MAX_HEALTH
+        println("Лечение игрока $name проведено. Текущий уровень здоровья: $health")
+    }
+}
+
+fun Player.isHealthy(): Boolean = this.health == MAX_HEALTH
+
+fun main() {
+    val player = Player("Duke Nukem", 70)
+    println("Текущее значение isHealthy: ${player.isHealthy()}")
+    player.healingPotion()
+    println("Текущее значение isHealthy: ${player.isHealthy()}")
+}
