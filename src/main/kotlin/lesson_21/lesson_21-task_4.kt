14897eee00d06c9c134f937beba89214d5dc6667
platon <platon@enod.xyz>
2023-10-13 02:23:09 +0500

    KS-21-4 - Extension File.saveTextToFile

--- src/main/kotlin/lesson_21/lesson_21-task_4.kt
+++ src/main/kotlin/lesson_21/lesson_21-task_4.kt
@@ -1,3 +1,16 @@
 package lesson_21
 
+import java.io.File
 
+fun File.saveTextToFile(text: String) {
+    this.createNewFile()
+    this.writeText(text.lowercase() + this.readText())
+}
+
+fun main() {
+    val file = File("word.txt")
+    file.saveTextToFile("Слово1")
+    println(file.readText())
+    file.saveTextToFile("Слово2")
+    println(file.readText())
+}
