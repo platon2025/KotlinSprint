209b4f0a99c8d72368430009a58b0f7ca685c0f6
platon <platon@enod.xyz>
2023-10-13 01:40:16 +0500

    KS-21-2 - List<Int>.evenNumbersSum()

--- src/main/kotlin/lesson_21/lesson_21-task_2.kt
+++ src/main/kotlin/lesson_21/lesson_21-task_2.kt
@@ -1,3 +1,12 @@
 package lesson_21
 
+fun List<Int>.evenNumbersSum(): Int {
+    var summ = 0
+    this.forEach { if (it % 2 == 0) summ += it }
+    return summ
+}
 
+fun main() {
+    val list = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 0)
+    println(list.evenNumbersSum())
+}
