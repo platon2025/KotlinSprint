8729479183e3d9a2785f90982b116149046749be
platon <platon@enod.xyz>
2023-10-12 20:02:36 +0500

    KS-21-1 - String.vowelCount

--- src/main/kotlin/lesson_21/lesson_21-task_1.kt
+++ src/main/kotlin/lesson_21/lesson_21-task_1.kt
@@ -1,3 +1,15 @@
 package lesson_21
 
+fun String.vowelCount(): Int {
+    val vowels = "EeUuIiOoAaАаОоУуЫыЭэЯяЁёЮюИиЕе"
+    var i = 0
+    for (a in this) {
+        if (vowels.indexOf(a) > -1) i++
+    }
+    return i
+}
 
+fun main() {
+    val string = "Welcome to Сочи!"
+    println("В строке \"$string\" содержится ${string.vowelCount()} гласных")
+}
