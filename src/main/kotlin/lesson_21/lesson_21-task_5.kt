f67621b427f06fe2b757274ee42bf48b4d1dd6a0
platon <platon@enod.xyz>
2023-10-14 04:24:43 +0500

    KS-21-5 - Extension Map.maxCategory()

--- src/main/kotlin/lesson_21/lesson_21-task_5.kt
+++ src/main/kotlin/lesson_21/lesson_21-task_5.kt
@@ -3,6 +3,6 @@ package lesson_21
 fun Map<String, Int>.maxCategory(): String = this.maxBy { it.value }.key
 
 fun main() {
-    val map = mapOf(Pair("здоровье", 90), Pair("сила", 100), Pair("скорость", 80))
+    val map = mapOf("здоровье" to 90, "сила" to 100, "скорость" to 80)
     println(map.maxCategory())
 }
