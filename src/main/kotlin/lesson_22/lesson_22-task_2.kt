4affcbf57c209f97b4063e939de1a355cdbd1a95
platon <platon@enod.xyz>
2023-10-16 08:33:06 +0300

    KS-22-2 - Data-class object output

--- src/main/kotlin/lesson_22/lesson_22-task_2.kt
+++ src/main/kotlin/lesson_22/lesson_22-task_2.kt
@@ -1,2 +1,12 @@
 package lesson_22
 
+class RegularBook1(val title: String, val author: String)
+data class DataBook1(val title: String, val author: String)
+
+fun main() {
+    val regBook = RegularBook1("Из ненаписанного", "А.С.Пушкин")
+    val dataBook = DataBook1("Из ненаписанного", "А.С.Пушкин")
+
+    println("Обычный класс: $regBook") //строковое представление объекта обычного класса выглядит как его уникальный id
+    println("Data класс: $dataBook") //строковое представление объекта data-класса выглядит как представление параметров объекта, переданных при его инициализации, в формате ["имя"="значение"]
+}
