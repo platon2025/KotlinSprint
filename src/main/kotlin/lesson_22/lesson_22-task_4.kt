d3a101ed15da919f95dca6b4adc7fa7a1fbc78ae
platon <platon@enod.xyz>
2023-11-19 18:33:08 +0300

    KS-22-4 - Data-class Copy

--- src/main/kotlin/lesson_22/lesson_22-task_4.kt
+++ src/main/kotlin/lesson_22/lesson_22-task_4.kt
@@ -1,2 +1,15 @@
 package lesson_22
 
+data class MainScreenState(val data: String, var isLoading: Boolean = false) {
+    init {
+        println("state = $data")
+    }
+}
+
+fun main() {
+    var screenState = MainScreenState("noData")
+    screenState = loadData(screenState, "dataLoading")
+    screenState = loadData(screenState, "dataLoaded")
+}
+
+fun loadData(screenState: MainScreenState, data: String): MainScreenState = screenState.copy(data = data)
