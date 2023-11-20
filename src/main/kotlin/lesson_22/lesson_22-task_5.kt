587f53d2f28c368a20a110e040e6188da0b8b25b
platon <platon@enod.xyz>
2023-11-20 09:42:43 +0300

    KS-22-5 - Data-class Alpha Centauri

--- src/main/kotlin/lesson_22/lesson_22-task_5.kt
+++ src/main/kotlin/lesson_22/lesson_22-task_5.kt
@@ -1,2 +1,36 @@
 package lesson_22
 
+import java.util.*
+
+fun main() {
+    val alphaCentauri = GalacticGuide(
+        "Alpha Centauri",
+        "Ближайшая к Земле звёздная система",
+        null,
+        4.367
+    )
+    alphaCentauri.printInfo()
+}
+
+data class GalacticGuide(
+    private val name: String,
+    private val description: String,
+    private val date: Long?, //timestamp
+    private val distance: Double,
+) {
+    fun printInfo() {
+        println("Название объекта/события: ${getName()}")
+        println("Описание: ${getDescription()}")
+        if (date != null) println("Дата события: ${(getDate())}")
+        println("Расстояние от Земли: ${getDistance()} св.лет")
+    }
+
+    fun getName(): String = name
+    fun getDescription(): String = description
+    fun getDate(): Date? {
+        return if (date != null) Date(date)
+        else null
+    }
+
+    fun getDistance(): Double = distance
+}
