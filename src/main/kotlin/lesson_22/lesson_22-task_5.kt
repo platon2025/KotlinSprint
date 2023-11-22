b2ca2ee34a5b1712f25b8306079f28b1151fb167
platon <platon@enod.xyz>
2023-11-22 10:29:05 +0300

    KS-22-5 - Data-class: Component functions

--- src/main/kotlin/lesson_22/lesson_22-task_5.kt
+++ src/main/kotlin/lesson_22/lesson_22-task_5.kt
@@ -6,17 +6,17 @@ fun main() {
     val guide = GalacticGuide(
         "Alpha Centauri",
         "Ближайшая к Земле звёздная система",
-        4.367,
         null,
+        4.367,
     )
     println("Название объекта/события: ${guide.component1()}")
     println("Описание: ${guide.component2()}")
-    println("Расстояние от Земли: ${guide.component3()} св.лет")
+    println("Расстояние от Земли: ${guide.component4()} св.лет")
 }
 
 data class GalacticGuide(
     val name: String,
     val description: String,
-    val distance: Double,
     val date: Long? = null,
+    val distance: Double,
 )
